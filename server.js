50a1f8c8f420b31f6d968699e29926822965b1fa
ibrahim-egy <ibrahimtarek.h@gmail.com>
2022-12-24 16:08:30 +0200

    set up a server

---
+++ server.js
@@ -0,0 +1,33 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// Start up an instance of app
+const app = express();
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Cors for cross origin allowance
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+
+// Get request to render index page
+app.get('/', (req, res)=> {
+    res.render('index.html')
+})
+
+
+
+const PORT = 3000
+
+app.listen(PORT, ()=> {
+    console.log(`Server up and running on port ${PORT}`);
+})
